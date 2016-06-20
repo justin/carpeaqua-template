9c43a19a85a63d8a3345abb7f51fad531b27c46d
Justin Williams <justinw@me.com>
2016-06-20 16:20:39 -0600

    Fixed inline code stylings.

--- 404.php
+++ 404.php
@@ -1,34 +1,11 @@
-<?php
-/**
- * The template for displaying 404 pages (not found)
- *
- * @package WordPress
- * @subpackage Twenty_Sixteen
- * @since Twenty Sixteen 1.0
- */
+<?php get_header(); ?>
 
-get_header(); ?>
+	<article class="post standard">
+		<div id="post-title">
+			<h1>Article not found</h1>
+		</div>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+		<p>It looks what you are looking for doesn't actually exist. Oops?</p>
+	</article>
 
-			<section class="error-404 not-found">
-				<header class="page-header">
-					<h1 class="page-title"><?php _e( 'Oops! That page can&rsquo;t be found.', 'twentysixteen' ); ?></h1>
-				</header><!-- .page-header -->
-
-				<div class="page-content">
-					<p><?php _e( 'It looks like nothing was found at this location. Maybe try a search?', 'twentysixteen' ); ?></p>
-
-					<?php get_search_form(); ?>
-				</div><!-- .page-content -->
-			</section><!-- .error-404 -->
-
-		</main><!-- .site-main -->
-
-		<?php get_sidebar( 'content-bottom' ); ?>
-
-	</div><!-- .content-area -->
-
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
--- template-parts/content.php
+++ template-parts/content.php
@@ -2,8 +2,8 @@
 
   <div id="post-title">
       <?php the_title( sprintf( '<h1><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
-        <time datetime="2016-05-01T17:15:00.000Z">May 01, 2016</time>
-      </div>
+      <time datetime="2016-05-01T17:15:00.000Z">May 01, 2016</time>
+  </div>
 
-		<?php the_content(); ?>
+	<?php the_content(); ?>
 </article>
