6844ab89bbfc96199fb5ed670cad587b447e097e
Justin Williams <justinw@me.com>
2016-06-21 19:15:03 -0600

    Actual dates.

--- template-parts/content.php
+++ template-parts/content.php
@@ -2,7 +2,7 @@
 
   <div id="post-title">
       <?php the_title( sprintf( '<h1><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
-      <time datetime="2016-05-01T17:15:00.000Z">May 01, 2016</time>
+      <time datetime="<?php the_time('c') ?>"><?php the_time('F jS, Y') ?></time>
   </div>
 
 	<?php the_content(); ?>
