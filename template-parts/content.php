8312d42482cf255b80f2df43fc68ff1e26f128ec
Justin Williams <justinw@me.com>
2017-03-20 14:54:30 -0600

    First pass

--- template-parts/content.php
+++
@@ -1,9 +0,0 @@
-<article class="post standard" id="post-<?php the_ID(); ?>">
-
-  <div id="post-title">
-      <?php the_title( sprintf( '<h1><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
-      <time datetime="<?php the_time('c') ?>"><?php the_time('F jS, Y') ?></time>
-  </div>
-
-	<?php the_content(); ?>
-</article>
