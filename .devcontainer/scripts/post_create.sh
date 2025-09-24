888a7c998d6b93ae0dbc791d840eb1430b44f1c5
Justin Williams <justinw@me.com>
2025-09-24 09:27:18 -0600

    Modernize devcontainer configuration

---
+++ .devcontainer/scripts/post_create.sh
@@ -0,0 +1,13 @@
+#/usr/bin/env sh
+
+set -eux
+
+echo "[setup_container] Starting container setup..."
+
+npm install -g ghost-cli@latest
+mkdir -p /home/node/ghost
+sudo chown -R node:node /home/node/ghost
+cd /home/node/ghost
+ghost install local --check-empty false
+
+echo "[setup_container] Done."
---
+++ .devcontainer/scripts/post_start.sh
@@ -0,0 +1,12 @@
+#/usr/bin/env sh
+
+set -eux
+
+echo "[setup_development] Configuring development environment..."
+
+cd /home/node/ghost/content/themes/carpeaqua-template
+npm install
+cd /home/node/ghost
+ghost start
+
+echo "[setup_development] Done."
